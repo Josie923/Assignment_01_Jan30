acbba3575c3e0b3e220142b9ef52bd08e8177054
Josie923 <yl3874@cornell.edu>
2025-02-02 17:05:27 -0500

    Add files via upload

---
+++ map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zaWUwOTIzMTAiLCJhIjoiY20xNDBuejgzMWo1bzJpcTJ1YjBjbXpncCJ9.0iHQV9BxlqBxfklfiR_lKQ';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/josie092310/cm6jjlk9q00bq01qqfgy82xr5',
+    center: [-73.956, 40.7628],
+    zoom: 11
+});
